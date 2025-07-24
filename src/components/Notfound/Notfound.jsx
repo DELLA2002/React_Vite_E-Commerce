11dc289bc64c1c4f3cb90626cc057d978cace974
mohamedabdellah <mohamedabdellah5492@gmail.com>
2025-07-25 01:19:36 +0300

    home

--- src/components/Notfound/Notfound.jsx
+++ src/components/Notfound/Notfound.jsx
@@ -3,11 +3,13 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import Style from './Notfound.module.css';
 import logo from '../../assets/notfound.jpg'
+import cart from '../../assets/logo.png'
 export default function Notfound() {
     return <>
         <div className="text-center">
-            <h1 className='pt-30 pb-4 text-3xl font-bold'>Page Not Found</h1>
-            <img src={logo} width={400} className='mx-auto' alt="" />
+            <h1 className='pt-10 text-3xl font-bold'>Welcome to Our WebSite</h1>
+            <img src={cart} width={400} className='mx-auto' alt="" />
+            {/* <img src={logo} width={800} className='mx-auto' alt="" /> */}
         </div> 
     </>
 }
